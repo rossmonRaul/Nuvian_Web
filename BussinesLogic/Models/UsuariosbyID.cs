b588c4af779736cb064a580765edc0aa919e30f9
Chepe05 <joseleiton0511@gamil.com>
2022-06-21 16:30:10 -0600

    Implementacion del API(editar usuarios, listar todos los usuarios y listar usuario por id)

---
+++ BussinesLogic/Models/UsuariosbyID.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinesLogic.Models
+{
+    public class UsuariosbyID
+    {
+        public int ID { get; set; }
+        public string contrasena { get; set; }
+        public string Nombre { get; set; }
+        public string Primer_apellido { get; set; }
+        public string Segundo_apellido { get; set; }
+        public string Correo { get; set; }
+        public int Telefono { get; set; }
+        public System.DateTime FechaNacimiento { get; set; }
+        public int ID_Aeropuerto { get; set; }
+    }
+}
