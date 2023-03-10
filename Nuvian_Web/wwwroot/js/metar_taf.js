74e75ebd9ac0c73c61bca349eed37fe85a3244a9
Sofia Vargas Garbanzo <sofia.vargasg@cuc.cr>
2023-03-10 15:59:00 -0600

    Implementación de widget metar

--- Nuvian_Web/wwwroot/js/metar_taf.js
+++ Nuvian_Web/wwwroot/js/metar_taf.js
@@ -1,7 +1,11 @@
 ﻿
+var indicador = false;
+
 
 $(document).ready(function () {
 	cargarcombo();
+
+
 })
 
 function cargarcombo() {
@@ -26,15 +30,80 @@ function CargarFrameParam() {
 
 	var aeropuerto = document.getElementById("slAeropuerto").value;
 
+	var clima1 = document.getElementById("clima");
 
 
-	var ruta = 'https://metar-taf.com/es/embed/' + aeropuerto + '?bg_color=0057a3&station_id=' + aeropuerto + '&layout=landscape';
+	var ruta = 'https://metar-taf.com/es/embed-js/' + aeropuerto + '?layout=landscape&target=BQSRpk5Y';
+	var link = 'https://metar-taf.com/es/' + aeropuerto + '?station_id=MROC';
 
-	var iframe = document.getElementById("frame1");
+	if (!indicador) {
 
-	iframe.setAttribute("src", ruta);
+		let script = document.createElement('script');
+
+		script.id = "script_widget"
+		script.async = true;
+		script.defer = true;
+		script.crossOrigin = "anonymous"
+		script.src = ruta;
 
 
 
+		clima.append(script);
 
+		let myHref = document.createElement("a");
+		myHref.setAttribute("href", "https://metar-taf.com/es/MRBP?station_id=MROC");
+		myHref.setAttribute("id", "metartaf-BQSRpk5Y");
+		myHref.setAttribute("style", "font-size:18px; font-weight:500; color:#000; width:350px; height:265px; display:block");
+		myHref.textContent = "METAR ";
+
+		clima1.appendChild(myHref);
+
+		script.onload = function () {
+
+
+		};
+
+		indicador = true;
+
+
+
+	} else {
+
+
+		var enlace = document.getElementById("metartaf-BQSRpk5Y");
+		var script1 = document.getElementById("script_widget");
+		var eliminar = clima1.removeChild(enlace, script1);
+
+		let script = document.createElement('script');
+
+		script.id = "script_widget"
+		script.async = true;
+		script.defer = true;
+		script.crossOrigin = "anonymous"
+		script.src = ruta;
+
+
+
+		clima.append(script);
+
+		let myHref = document.createElement("a");
+		myHref.setAttribute("href", link);
+		myHref.setAttribute("id", "metartaf-BQSRpk5Y");
+		myHref.setAttribute("style", "font-size:18px; font-weight:500; color:#000; width:350px; height:265px; display:block");
+		myHref.textContent = "METAR ";
+
+		clima1.appendChild(myHref);
+
+		script.onload = function () {
+
+
+		};
+
+		indicador = true;
+
+	}
 }
+
+
+
+
